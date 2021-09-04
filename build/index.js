3611cc8394da0bda1f92dee152da47bcd1b2ae65
Justin Patterson <patte2jd@gmail.com>
2021-09-03 22:57:26 -0400

    Fixed enum exporting

--- build/index.js
+++ build/index.js
@@ -1,7 +1,5 @@
-import { DISPLAY_FORMAT_TYPE } from './models/models';
 import { formatData } from './data-formatter';
 export var DataFormatter;
 (function (DataFormatter) {
-    DataFormatter.FORMAT_TYPE = DISPLAY_FORMAT_TYPE;
     DataFormatter.format = formatData;
 })(DataFormatter || (DataFormatter = {}));
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { DISPLAY_FORMAT_TYPE } from './models/models';
 import { formatData } from './data-formatter';
 
 export namespace DataFormatter {
-  export const FORMAT_TYPE = DISPLAY_FORMAT_TYPE;
+  export type FORMAT_TYPE = DISPLAY_FORMAT_TYPE;
 
   export const format = formatData;
 }
